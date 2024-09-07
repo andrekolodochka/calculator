b0bc3d4b919e050560312bafc5252f00814e30cd
Andre Kolodochka <andre@andrek.com>
2024-09-07 14:20:31 +1000

    another

--- oops/IncompleteUrlSubstringSanitization.js
+++ oops/IncompleteUrlSubstringSanitization.js
@@ -1,7 +1,8 @@
 app.get('/some/path', function(req, res) {
-    let url = req.param("url");
+    let url = req.param("url"),
+        host = urlLib.parse(url).host;
     // BAD: the host of `url` may be controlled by an attacker
-    if (url.includes("example.com")) {
+    if (host.includes("example.com")) {
         res.redirect(url);
     }
 });
